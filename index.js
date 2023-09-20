0134d4bf7b4e6945600aa3cedaea0dd7f6d2cd45
Maksim <max-goryachev@ya.ru>
2023-09-20 19:57:58 +0600

    some commits

---
+++ index.js
@@ -0,0 +1,19 @@
+autoSlider()
+let offset = 0
+
+
+const sliderLine = document.querySelector('.slider-container')
+
+function slider() {
+
+    offset = offset + 50
+    if (offset > 150) { offset = 0 }
+    sliderLine.style.bottom = offset + 'px'
+    autoSlider()
+}
+function autoSlider() {
+    let timer;
+    timer = setTimeout(slider, 2000)
+
+    console.log(1)
+}
